a73449848ce85cb62c81ed89451847b598ac4a14
Marko B <mb1135@student.uni.lj.si>
2020-01-10 13:15:40 +0100

    Server button functionality added.

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -2,11 +2,7 @@
   $("#seznam").append("<tr><td>" + element.ime + "</td><td>" + element.starost + "</td></tr>");
 }
 
-function localButton() {
-  const starost = $("#starost").val();
-  const ime = $("#ime").val();
-  
-  console.log(starost);
+function checkFields(starost, ime){
   if(starost==""){
     $("#starost-error").show();
     console.log("STAROST");
@@ -21,7 +17,14 @@ function localButton() {
     $("#ime-error").hide();
   }
 
-  if(ime != "" && starost != ""){
+  return ime != "" && starost != "";
+}
+
+function localButton() {
+  const starost = $("#starost").val();
+  const ime = $("#ime").val();
+
+  if(checkFields(starost, ime)){
     var element = {};
     element.starost=starost;
     element.ime = ime;
@@ -29,12 +32,44 @@ function localButton() {
     $("#starost").val("");
     $("#ime").val("");
   }
-  console.log("str: " + starost + ", ime: " + ime);
+}
+
+function serverButton() {
+  const starost = $("#starost").val();
+  const ime = $("#ime").val();
+
+  if(checkFields(starost, ime)){
+    var element = {};
+    element.starost=starost;
+    element.ime = ime;
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:50514/api/osebe",
+      // The key needs to match your method's input parameter (case-sensitive).
+      data: JSON.stringify(element),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        console.log(data);
+        var data_element = {starost: data.starost, ime: data.ime};
+        addToTable(data_element);
+      },
+      failure: function(errMsg) {
+          alert(errMsg);
+      }
+    });
+    $("#starost").val("");
+    $("#ime").val("");
+  }
 }
 
 $(document).ready(function(){
     $("#lokalno").click(function(){
       localButton();
+    });
+
+    $("#streznik").click(function() {
+      serverButton();
     });
 
 
